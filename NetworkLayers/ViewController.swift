af56ebe29148149de703acd25643ad601772d62c
patrick Wiley <wileypatrick36@yahoo.com>
2020-05-15 10:58:07 +0200

    Initial Commit

---
+++ NetworkLayers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLayers
+//
+//  Created by Patrick Wiley on 15.05.20.
+//  Copyright © 2020 Patrick Wiley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
